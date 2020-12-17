30735aef5c9ec213d2dbf13455f20fadb29537a8
yumminny <51070291+yumminny@users.noreply.github.com>
2020-12-17 17:42:48 -0500

    Update sunClock.py

--- sunClock.py
+++ sunClock.py
@@ -5,15 +5,12 @@ import datetime as dt
 import math
 import sys
 
-for arg in sys.argv:
-    print(arg)
-
 options = RGBMatrixOptions()
 options.rows = 32
 options.chain_length = 1
 options.cols = 64
 options.disable_hardware_pulsing = 0
-options.gpio_slowdown = 4
+options.gpio_slowdown = 3
 options.brightness = 100
 options.pwm_bits = 11
 options.hardware_mapping="adafruit-hat"
@@ -21,16 +18,29 @@ options.hardware_mapping="adafruit-hat"
 
 matrix = RGBMatrix(options = options)
 print ("Matrix initialized")
-#starting point
 
+#converting strings into sunrise time object
+myString1 = sys.argv[1].replace('"','')
+mySunRise = myString1.split(':')
+tRh = int(mySunRise[0])
+tRm = int(mySunRise[1])
+tRs = int(mySunRise[2])
+tR = dt.time(tRh, tRm, tRs)
+#converting strings into sunset time object
+myString2 = sys.argv[2].replace('"','')
+mySunSet = myString2.split(':')
+tSh = int(mySunSet[0])
+tSm = int(mySunSet[1])
+tSs = int(mySunSet[2])
+tS = dt.time(tSh, tSm, tSs)
 
-#sunrise time
-tR = dt.time(7,23,14)
-#sunset time
-tS = dt.time(17,31,23)
+#manual input sunrise time
+#tR = dt.time(7,9,14)
+#manual input sunset time
+#tS = dt.time(16,28,23)
+
 #now
-tN = datetime.now()
-#tN = dt.time(14,23,24)
+tN = dt.time(4,30,30)#datetime.now()
 #sum of time
 tRS = tR.hour * 3600 + tR.minute * 60 + tR.second 
 tSS = tS.hour * 3600 + tS.minute * 60 + tS.second 
@@ -47,13 +57,13 @@ print(currentSunLoc)
 
 cenX = 64 * 0.5
 cenY = 32 * 1.5 
-sunRad = 10
-sunPath = 32
+sunRad = 8
+sunPath = 35
 totalSunPathAngle = 2.21429744
 sunAngle = currentSunLoc * totalSunPathAngle
+
 print(sunAngle)
 curAngle = angle + sunAngle
-
 while (True):
     now = datetime.now()
     #print(now)
@@ -70,7 +80,7 @@ while (True):
     #matrix.SetPixel(pX, pY,0,0,0)
 
     #move the sun
-    angle += sunMoveRate
+    curAngle += .05
     #draw current pixel on path
     cX = cenX + math.cos(curAngle) * sunPath
     cY = cenY + math.sin(curAngle) * sunPath
@@ -79,13 +89,19 @@ while (True):
     #print(cX," ",cY)
     #print("running pixels")
 
+    distanceFromMiddle = math.sqrt(((cX-32)**2) + ((cY-20)**2))
+    print(distanceFromMiddle)
 
     for j in range(0, 64):
         for i in range (0,32):
             distance = math.sqrt( ((cX-j)**2)+((cY-i)**2))
-            if(distance < sunRad):
+            if(distance < sunRad+distanceFromMiddle*.1):
                 matrix.SetPixel(j,i,255,255,255)
             else:
                 matrix.SetPixel(j,i,0,0,0)
 
     sleep(.1)
+
+
+
+
